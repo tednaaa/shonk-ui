f325ee938668a12f2b1f8a63fd43f30f9b00fcbc
Andranik <tednaaa@gmail.com>
2026-08-08 16:28:10 +0400

    remove useless packages

--- devenv.nix
+++ devenv.nix
@@ -6,4 +6,9 @@
       package = pkgs.nodejs_24;
     };
   };
+
+  scripts = {
+    knip.exec = "pnpm dlx knip";
+    update_npm_packages.exec = "pnpm dlx npm-check-updates -i --format group";
+  };
 }
