7549bb2793705c44f4e8ddbd2722908f7c738c78
Andranik <tednaaa@gmail.com>
2026-09-23 18:08:21 +0400

    fix(table): center a switch vertically in a table cell

--- src/components/table/TableCell.vue
+++ src/components/table/TableCell.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     data-slot="table-cell"
     :class="
       cn(
-        'p-2 align-middle whitespace-nowrap has-[[role=checkbox]]:pr-0 *:[[role=checkbox]]:translate-y-[2px]',
+        'p-2 align-middle whitespace-nowrap has-[[role=checkbox]]:pr-0 has-[>[role=switch]]:leading-none *:[[role=checkbox]]:translate-y-[2px] *:[[role=switch]]:align-middle',
         props.class,
       )
     "
